d80139445262297eeda5b8b1dc69c7e10c0b3f7f
benyaminpravalent <pravalentbenyamin@gmail>
2025-01-03 01:17:20 +0400

    chore: update dto for swagger

--- src/location/dtos/location-output.dto.ts
+++ src/location/dtos/location-output.dto.ts
@@ -1,15 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 
 export class LocationOutput {
   @Expose()
+  @ApiProperty()
   id: number;
 
   @Expose()
+  @ApiProperty()
   userId: number;
 
   @Expose()
+  @ApiProperty()
   cityName: string;
 
   @Expose()
+  @ApiProperty()
   createdAt: Date;
 }
